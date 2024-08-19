bc06381fd3473fbfd200cd87304146ba3a72fcf3
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-08-19 19:58:14 +0530

    Updated some dependency

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,9 +3,11 @@
 import { useBalance } from "@repo/store/useBalance";
 
 export default function() {
+  console.log("Reached here")
   const balance = useBalance();
   return (
     <div>
+      Hello from Merchant App
       hi there {balance}
     </div>
   )
--- apps/merchant-app/provider.tsx
+++ apps/merchant-app/provider.tsx
@@ -3,11 +3,10 @@
 import { RecoilRoot } from "recoil"
 
 export const Providers = ({children}: {children: React.ReactNode}) => {
+    console.log("Recoil Root is being rendered")
     return (
         <RecoilRoot>
-            <body>
-                {children}
-            </body>
+            {children}
         </RecoilRoot>
     )
 }
