54270f8d7d4353d47a9f2e2887fe0c196a785ee9
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-08-20 23:36:19 +0530

    Minor changes

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { PrismaClient } from "@repo/db/client";
 
 
 export default function Home() {
