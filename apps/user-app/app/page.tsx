340b74db7f935a1ef4b7ae7c3e2f7ff7d47b2402
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-09-28 23:34:19 +0530

    Added dashboard and the components

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,25 @@
-"use client"
+// "use client"
 
-import { Appbar } from "@repo/ui/appbar";
-import { signIn, signOut, useSession } from "next-auth/react";
+// import { Appbar } from "@repo/ui/appbar";
+import { getServerSession } from "next-auth";
+// import { signIn, signOut, useSession } from "next-auth/react";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Page(): JSX.Element {
-  const session = useSession();
-  return (
-    <div className="text-2xl">
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-    </div>
-  );
+// export default function Page(): JSX.Element {
+//   const session = useSession();
+//   return (
+//     <div className="text-2xl">
+//       <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+//     </div>
+//   );
+// }
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  redirect('/dashboard')
+  // if (session?.user) {
+  //   redirect('/dashboard')
+  // } else {
+  //   redirect('/api/auth/signin')
+  // }
 }
