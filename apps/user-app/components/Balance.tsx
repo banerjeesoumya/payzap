c1e170b6e62e7d2abf83cb051cf3c6bc1f0cbbeb
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-09-28 23:48:32 +0530

    Added the balance card component

---
+++ apps/user-app/components/Balance.tsx
@@ -0,0 +1,13 @@
+import { useBalance } from "@repo/store/useBalance";
+
+function Balance() {
+    const value = useBalance();
+    console.log(value);
+    return (
+        <p>
+            The current balance is {value}
+        </p>
+    )
+}
+
+export default Balance;
